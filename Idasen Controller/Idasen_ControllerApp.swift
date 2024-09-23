ee20d969f860ff46cb2e1e90cd48a0ba9a78e5b4
Marcell Schuh <marcell@mntr.dev>
2024-09-23 21:19:55 +0200

    Initial Commit

---
+++ Idasen Controller/Idasen_ControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Idasen_ControllerApp.swift
+//  Idasen Controller
+//
+//  Created by Marcell Schuh on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Idasen_ControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Idasen ControllerTests/Idasen_ControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Idasen_ControllerTests.swift
+//  Idasen ControllerTests
+//
+//  Created by Marcell Schuh on 9/23/24.
+//
+
+import Testing
+@testable import Idasen_Controller
+
+struct Idasen_ControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
